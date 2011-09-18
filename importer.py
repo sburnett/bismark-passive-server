8af207ec193321b3201d39f1b022fb1e4c53a757
Sam Burnett <collage@gtnoise.net>
2011-09-18 16:58:14 -0400

    Schema and importer done but untested

--- importer.py
+++ importer.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy.sql.expression import desc 
+from sqlalchemy.sql.expression import desc
 
 import schema
 
@@ -7,113 +7,87 @@ class PassiveUpdateImporter(object):
     def __init__(self, session):
         self._session = session
 
-    def _insert_mac_address(self, session, data):
-        mac_address = self._session.query(schema.MacAddress).\
-                filter_by(session=session, data=data).\
-                first()
-        if not mac_address:
-            mac_address = schema.MacAddress(session=session, data=data)
-            self._session.add(mac_address)
-        #self._session.flush()
-        return mac_address
+    def _insert_entry(self, Cl, **kwargs):
+        ob = self._session.query(Cl).filter_by(**kwargs).first()
+        if not ob:
+            ob = Cl(**kwargs)
+            self._session.add(ob)
+        return ob
 
-    def _insert_ip_address(self, session, data):
-        ip_address = self._session.query(schema.IpAddress).\
-                filter_by(session=session, data=data).\
-                first()
-        if not ip_address:
-            ip_address = schema.IpAddress(session=session, data=data)
-            self._session.add(ip_address)
-        #self._session.flush()
-        return ip_address
+    def _insert_mac_address(self, anonymization_context, data):
+        return self._insert_entry(schema.MacAddress,
+                                  anonymization_context=anonymization_context,
+                                  data=data)
 
-    def _insert_domain_name(self, session, data):
-        domain_name = self._session.query(schema.DomainName).\
-                filter_by(session=session, data=data).\
-                first()
-        if not domain_name:
-            domain_name = schema.DomainName(session=session, data=data)
-            self._session.add(domain_name)
-        #self._session.flush()
-        return domain_name
+    def _insert_ip_address(self, anonymization_context, data):
+        return self._insert_entry(schema.IpAddress,
+                                  anonymization_context=anonymization_context,
+                                  data=data)
+
+    def _insert_domain_name(self, anonymization_context, data):
+        return self._insert_entry(schema.DomainName,
+                                  anonymization_context=anonymization_context,
+                                  data=data)
 
     def _insert_local_address(
-            self, update, remote_address_id, mac_address, ip_address):
-        local_address = self._session.query(schema.LocalAddress).\
-                filter_by(update=update,
-                          mac_address=mac_address,
-                          ip_address=ip_address).\
-                first()
-        if not local_address:
-            local_address = schema.LocalAddress(
-                    update=update,
-                    remote_address_id=remote_address_id,
-                    mac_address=mac_address,
-                    ip_address=ip_address)
-            self._session.add(local_address)
-        local_address.remote_address_id = remote_address_id
+            self, remote_id, update, mac_address, ip_address):
+        local_address = self._insert_entry(
+                schema.LocalAddress,
+                update=update,
+                mac_address=mac_address,
+                ip_address=ip_address)
+        local_address.remote_id = remote_id
         self._session.merge(local_address)
-        #self._session.flush()
         return local_address
 
-    def _insert_dns_mapping(self, update, local_address, domain_name, ip_address):
-        dns_mapping = self._session.query(schema.DnsMapping).\
-                filter_by(update=update,
-                          local_address=local_address,
-                          domain_name=domain_name,
-                          ip_address=ip_address).\
-                first()
-        if not dns_mapping:
-            dns_mapping = schema.DnsMapping(
-                    update=update,
-                    local_address=local_address,
-                    domain_name=domain_name,
-                    ip_address=ip_address)
-            self._session.add(dns_mapping)
-        #self._session.flush()
-        return dns_mapping
+    def _insert_dns_a_record(self, update, local_address, domain_name, ip_address):
+        return self._insert_entry(schema.DnsARecord,
+                                  update=update,
+                                  local_address=local_address,
+                                  domain_name=domain_name,
+                                  ip_address=ip_address)
+
+    def _insert_dns_cname_record(self, update, local_address, domain_name, cname):
+        return self._insert_entry(schema.DnsCnameRecord,
+                                  update=update,
+                                  local_address=local_address,
+                                  domain_name=domain_name,
+                                  cname=cname)
+
+    def _lookup_local_addresses_for_session(self, session):
+        local_addresses = self._session.\
+                query(schema.LocalAddress).join(schema.Update).\
+                filter(schema.Update.session == session).\
+                order_by(desc(schema.Update.sequence_number)).\
+                distinct(schema.LocalAddress.remote_id)
+        return dict(map(lambda la: (la.remote_id, la), local_addresses))
+
+    def _lookup_flows_for_session(self, session):
+        flows = self._session.\
+                query(schema.Flow).\
+                join(schema.Update).\
+                filter(schema.Update.session == session).\
+                order_by(desc(schema.Update.sequence_number)).\
+                distinct(schema.Flow.remote_id)
+        return dict(map(lambda f: (f.remote_id, f), flows))
 
     def import_update(self, parsed_update):
-        print 'HEADER'
-
-        node = self._session.query(schema.Node).\
-                filter_by(name=parsed_update.bismark_id).\
-                first()
-        if not node:
-            node = schema.Node(name=parsed_update.bismark_id)
-            self._session.add(node)
-
+        node = self._insert_entry(schema.Node, name=parsed_update.bismark_id)
         if parsed_update.anonymized:
             signature = parsed_update.anonymization_signature
         else:
             signature = ''
-        anonymization_context = self._session.query(schema.AnonymizationContext).\
-                filter_by(node=node, signature=signature).\
-                first()
-        if not anonymization_context:
-            anonymization_context = schema.AnonymizationContext(
-                    node=node,
-                    signature=signature)
-            self._session.add(anonymization_context)
-
-        session = self._session.query(schema.Session).\
-                filter_by(anonymization_context=anonymization_context,
-                          key=parsed_update.creation_time).\
-                first()
-        if not session:
-            session = schema.Session(anonymization_context=anonymization_context,
-                                     key=parsed_update.creation_time)
-            self._session.add(session)
-
-        update = self._session.query(schema.Update).\
-                filter_by(session=session,
-                          sequence_number=parsed_update.sequence_number).\
-                first()
-        if not update:
-            update = schema.Update(
-                    session=session,
-                    sequence_number=parsed_update.sequence_number)
-            self._session.add(update)
+        anonymization_context = self._insert_entry(schema.AnonymizationContext,
+                                                   node=node,
+                                                   signature=signature)
+        session = self._insert_entry(
+                schema.Session,
+                anonymization_context=anonymization_context,
+                key=parsed_update.creation_time)
+        update = self._insert_entry(
+                schema.Update,
+                session=session,
+                sequence_number=parsed_update.sequence_number)
         try:
             update.pcap_received = parsed_update.pcap_received
             update.pcap_dropped = parsed_update.pcap_dropped
@@ -122,79 +96,55 @@ class PassiveUpdateImporter(object):
         except:
             pass
 
-        print 'LOCAL ADDRESSES'
-
         for idx, entry in enumerate(parsed_update.addresses):
-            mac_address = self._insert_mac_address(session, entry.mac_address)
-            ip_address = self._insert_ip_address(session, entry.ip_address)
+            mac_address = self._insert_mac_address(anonymization_context,
+                                                   entry.mac_address)
+            ip_address = self._insert_ip_address(anonymization_context,
+                                                 entry.ip_address)
             remote_id = (parsed_update.address_table_first_id + idx) \
                     % parsed_update.address_table_size
             local_address = self._insert_local_address(
-                    update, remote_id, mac_address, ip_address)
+                    remote_id, update, mac_address, ip_address)
 
-        print 'A RECORDS'
-
-        local_addresses = self._session.\
-                query(schema.LocalAddress).join(schema.Update).\
-                filter(schema.Update.session == session).\
-                order_by(desc(schema.Update.sequence_number)).\
-                distinct(schema.LocalAddress.remote_address_id)
-        local_address_map = dict(map(lambda la: (la.remote_address_id, la),
-                                     local_addresses))
-
+        local_addresses = self._lookup_local_addresses_for_session(session)
         for entry in parsed_update.a_records:
-            local_address = local_address_map[entry.address_id]
-            domain_name = self._insert_domain_name(session, entry.domain)
-            ip_address = self._insert_ip_address(session, entry.ip_address)
-            dns_mapping = self._insert_dns_mapping(update,
-                                                   local_address,
-                                                   domain_name,
-                                                   ip_address)
-
-        print 'CNAME RECORDS'
-
+            local_address = local_addresses[entry.address_id]
+            domain_name = self._insert_domain_name(anonymization_context,
+                                                   entry.domain)
+            ip_address = self._insert_ip_address(anonymization_context,
+                                                 entry.ip_address)
+            self._insert_dns_a_record(update,
+                                      local_address,
+                                      domain_name,
+                                      ip_address)
         for entry in parsed_update.cname_records:
-            local_address = local_address_map[entry.address_id]
-            dns_mappings = self._session.\
-                    query(schema.DnsMapping).join(schema.DomainName).\
-                    filter(schema.DnsMapping.local_address == local_address).\
-                    filter(schema.DomainName.data == entry.cname)
-            domain_name = self._insert_domain_name(session, entry.domain)
-            for dns_mapping in dns_mappings:
-                self._insert_dns_mapping(update,
-                                         dns_mapping.local_address,
-                                         domain_name,
-                                         dns_mapping.ip_address)
-
-        print 'FLOW TABLE'
+            local_address = local_addresses[entry.address_id]
+            domain_name = self._insert_domain_name(anonymization_context,
+                                                   entry.domain)
+            cname = self._insert_domain_name(anonymization_context,
+                                             entry.cname)
+            self._insert_dns_cname_record(update,
+                                          local_address,
+                                          domain_name,
+                                          cname)
 
         for entry in parsed_update.flow_table:
-            source_ip = self._insert_ip_address(session, entry.source_ip)
-            destination_ip = self._insert_ip_address(session,
+            source_ip = self._insert_ip_address(anonymization_context,
+                                                entry.source_ip)
+            destination_ip = self._insert_ip_address(anonymization_context,
                                                      entry.destination_ip)
-            flow = schema.Flow(remote_flow_id=entry.flow_id,
+            flow = schema.Flow(remote_id=entry.flow_id,
                                update=update,
                                source_ip=source_ip,
                                destination_ip=destination_ip,
-                               transport=entry.transport_protocol,
+                               transport_protocol=entry.transport_protocol,
                                source_port=entry.source_port,
                                destination_port=entry.destination_port)
             self._session.add(flow)
 
-        print 'PACKET SERIES'
-
-        flow_ids = map(lambda entry: entry.flow_id, parsed_update.packet_series)
-
-        flows = self._session.\
-                query(schema.Flow).\
-                join(schema.Update).\
-                filter(schema.Update.session == session).\
-                order_by(desc(schema.Update.sequence_number)).\
-                distinct(schema.Flow.remote_flow_id)
-        flowmap = dict(map(lambda f: (f.remote_flow_id, f), flows))
-
+        flows = self._lookup_flows_for_session(session)
         for entry in parsed_update.packet_series:
-            flow = flowmap.get(entry.flow_id)
+            flow = flows.get(entry.flow_id)
             timestamp = datetime.utcfromtimestamp(entry.timestamp / 1e6)
             packet = schema.Packet(timestamp=timestamp,
                                    size=entry.size,
--- schema.py
+++ schema.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, create_engine, ForeignKey, MetaData
-from sqlalchemy import BigInteger, DateTime, Integer, String
+from sqlalchemy import BigInteger, Boolean, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import backref, relationship, sessionmaker
 from sqlalchemy.schema import UniqueConstraint
@@ -7,11 +7,14 @@ from sqlalchemy.schema import UniqueConstraint
 Base = declarative_base()
 
 class Node(Base):
+    """Represents a router in a home."""
     __tablename__ = 'nodes'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
 
 class AnonymizationContext(Base):
+    """Represents an anonymization key on a router; for each router there may be
+    many keys over time."""
     __tablename__ = 'anonymization_contexts'
     __table_args__ = (UniqueConstraint('node_id', 'signature'), )
 
@@ -22,6 +25,7 @@ class AnonymizationContext(Base):
     node = relationship(Node, backref='anonymization_contexts')
 
 class Session(Base):
+    """Represents a session of the bismark-passive client."""
     __tablename__ = 'sessions'
     __table_args__ = (UniqueConstraint('anonymization_context_id', 'key'), )
 
@@ -35,6 +39,7 @@ class Session(Base):
                                          backref='sessions')
 
 class Update(Base):
+    """Represents a single update received from a bismark-passive client."""
     __tablename__ = 'updates'
     __table_args__ = (UniqueConstraint('session_id', 'sequence_number'), )
 
@@ -49,23 +54,42 @@ class Update(Base):
 
 class MacAddress(Base):
     __tablename__ = 'mac_addresses'
-    __table_args__ = (UniqueConstraint('session_id', 'data'), )
+    __table_args__ = (UniqueConstraint('anonymization_context_id', 'data'), )
 
     id = Column(Integer, primary_key=True)
-    session_id = Column(Integer, ForeignKey('sessions.id'), nullable=False)
+    anonymization_context_id = Column(Integer,
+                                      ForeignKey('anonymization_contexts.id'),
+                                      nullable=False)
     data = Column(BigInteger, nullable=False)
 
-    session = relationship(Session, backref='mac_addresses')
+    anonymization_context = relationship(AnonymizationContext,
+                                         backref='mac_addresses')
 
 class IpAddress(Base):
     __tablename__ = 'ip_addresses'
-    __table_args__ = (UniqueConstraint('session_id', 'data'), )
+    __table_args__ = (UniqueConstraint('anonymization_context_id', 'data'), )
 
     id = Column(Integer, primary_key=True)
-    session_id = Column(Integer, ForeignKey('sessions.id'), nullable=False)
+    anonymization_context_id = Column(Integer,
+                                      ForeignKey('anonymization_contexts.id'),
+                                      nullable=False)
     data = Column(Integer, nullable=False)
 
-    session = relationship(Session, backref='ip_addresses')
+    anonymization_context = relationship(AnonymizationContext,
+                                         backref='ip_addresses')
+
+class DomainName(Base):
+    __tablename__ = 'domain_names'
+    __table_args__ = (UniqueConstraint('anonymization_context_id', 'data'), )
+
+    id = Column(Integer, primary_key=True)
+    anonymization_context_id = Column(Integer,
+                                      ForeignKey('anonymization_contexts.id'),
+                                      nullable=False)
+    data = Column(String(256), nullable=False)
+
+    anonymization_context = relationship(AnonymizationContext,
+                                         backref='domain_names')
 
 class LocalAddress(Base):
     __tablename__ = 'local_addresses'
@@ -74,27 +98,21 @@ class LocalAddress(Base):
                                        'update_id'), )
 
     id = Column(Integer, primary_key=True)
+    remote_id = Column(Integer, nullable=False)
     update_id = Column(Integer, ForeignKey('updates.id'), nullable=False)
-    remote_address_id = Column(Integer, nullable=False)
-    mac_address_id = Column(Integer, ForeignKey('mac_addresses.id'), nullable=False)
-    ip_address_id = Column(Integer, ForeignKey('ip_addresses.id'), nullable=False)
+    mac_address_id = Column(Integer,
+                            ForeignKey('mac_addresses.id'),
+                            nullable=False)
+    ip_address_id = Column(Integer,
+                           ForeignKey('ip_addresses.id'),
+                           nullable=False)
 
     update = relationship(Update, backref='local_addresses')
     mac_address = relationship(MacAddress, backref='local_addresses')
     ip_address = relationship(IpAddress, backref='local_addresses')
 
-class DomainName(Base):
-    __tablename__ = 'domain_names'
-    __table_args__ = (UniqueConstraint('session_id', 'data'), )
-
-    id = Column(Integer, primary_key=True)
-    session_id = Column(Integer, ForeignKey('sessions.id'), nullable=False)
-    data = Column(String(256), nullable=False)
-
-    session = relationship(Session, backref='domain_names')
-
-class DnsMapping(Base):
-    __tablename__ = 'dns_mappings'
+class DnsARecord(Base):
+    __tablename__ = 'dns_a_records'
     __table_args__ = (UniqueConstraint('update_id',
                                        'local_address_id',
                                        'domain_name_id',
@@ -102,23 +120,59 @@ class DnsMapping(Base):
 
     id = Column(Integer, primary_key=True)
     update_id = Column(Integer, ForeignKey('updates.id'), nullable=False)
-    local_address_id = Column(Integer, ForeignKey('local_addresses.id'), nullable=False)
-    domain_name_id = Column(Integer, ForeignKey('domain_names.id'), nullable=False)
-    ip_address_id = Column(Integer, ForeignKey('ip_addresses.id'), nullable=False)
+    local_address_id = Column(Integer,
+                              ForeignKey('local_addresses.id'),
+                              nullable=False)
+    domain_name_id = Column(Integer,
+                            ForeignKey('domain_names.id'),
+                            nullable=False)
+    ip_address_id = Column(Integer,
+                           ForeignKey('ip_addresses.id'),
+                           nullable=False)
 
-    update = relationship(Update, backref='dns_mappings')
-    local_address = relationship(LocalAddress, backref='dns_mappings')
-    domain_name = relationship(DomainName, backref='dns_mappings')
-    ip_address = relationship(IpAddress, backref='dns_mappings')
+    update = relationship(Update, backref='dns_a_records')
+    local_address = relationship(LocalAddress, backref='dns_a_records')
+    domain_name = relationship(DomainName, backref='dns_a_records')
+    ip_address = relationship(IpAddress, backref='dns_a_records')
+
+class DnsCnameRecord(Base):
+    __tablename__ = 'dns_cname_records'
+
+    id = Column(Integer, primary_key=True)
+    update_id = Column(Integer, ForeignKey('updates.id'), nullable=False)
+    local_address_id = Column(Integer,
+                              ForeignKey('local_addresses.id'),
+                              nullable=False)
+    domain_name_id = Column(Integer,
+                            ForeignKey('domain_names.id'),
+                            nullable=False)
+    cname_id = Column(Integer,
+                      ForeignKey('domain_names.id'),
+                      nullable=False)
+
+    update = relationship(Update, backref='dns_cname_records')
+    local_address = relationship(LocalAddress, backref='dns_cname_records')
+    domain_name = relationship(DomainName,
+                               backref='dns_cname_domains',
+                               primaryjoin=domain_name_id == DomainName.id)
+    cname = relationship(DomainName,
+                         backref='dns_cname_records',
+                         primaryjoin=cname_id == DomainName.id)
 
 class Flow(Base):
     __tablename__ = 'flows'
+    __table_args__ = (UniqueConstraint('remote_id', 'update_id'), )
+
     id = Column(Integer, primary_key=True)
-    remote_flow_id = Column(Integer, nullable=False)
+    remote_id = Column(Integer, nullable=False)
     update_id = Column(Integer, ForeignKey('updates.id'), nullable=False)
-    source_ip_id = Column(Integer, ForeignKey('ip_addresses.id'), nullable=False)
-    destination_ip_id = Column(Integer, ForeignKey('ip_addresses.id'), nullable=False)
-    transport = Column(Integer, nullable=False)
+    source_ip_id = Column(Integer,
+                          ForeignKey('ip_addresses.id'),
+                          nullable=False)
+    destination_ip_id = Column(Integer,
+                               ForeignKey('ip_addresses.id'),
+                               nullable=False)
+    transport_protocol = Column(Integer, nullable=False)
     source_port = Column(Integer, nullable=False)
     destination_port = Column(Integer, nullable=False)
 
