d16177f25e79d54e33cd1abb60c436fb8f396290
Sam Burnett <collage@gtnoise.net>
2011-09-22 01:41:13 -0400

    Ditch sqlalchemy; using Postgresql turns out to be much easier and faster

---
+++ db.py
@@ -0,0 +1,148 @@
+import psycopg2
+
+class BismarkPassiveDatabase(object):
+    merge_node_text = 'SELECT merge_node (%s)'
+    merge_anonymization_context_text = \
+            'SELECT merge_anonymization_context (%s, %s)'
+    merge_session_text = 'SELECT merge_session (%s, %s)'
+    merge_update_text = \
+            'SELECT merge_update (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    merge_mac_address_text = 'SELECT merge_mac_address (%s, %s)'
+    merge_ip_address_text = 'SELECT merge_ip_address (%s, %s)'
+    merge_domain_name_text = 'SELECT merge_domain_name (%s, %s)'
+    merge_local_address_text = 'SELECT merge_local_address (%s, %s, %s, %s)'
+    merge_dns_a_record_text = 'SELECT merge_dns_a_record (%s, %s, %s, %s)'
+    merge_dns_cname_record_text = \
+            'SELECT merge_dns_cname_record (%s, %s, %s, %s)'
+    merge_flow_text = 'SELECT merge_flow (%s, %s, %s, %s, %s, %s, %s)'
+    merge_packet_text = 'SELECT merge_packet (%s, %s, timestamp %s, %s)'
+    lookup_local_address_for_session = \
+            'SELECT lookup_local_address_for_session (%s, %s)'
+    lookup_flow_for_session = \
+            'SELECT lookup_flow_for_session (%s, %s)'
+
+    def __init__(self, connection_string):
+        self._conn = psycopg2.connect(connection_string)
+
+    def _execute_command(self, command, *args):
+        cur = self._conn.cursor()
+        cur.execute(command, args)
+        result = cur.fetchone()[0]
+        cur.close()
+        return result
+
+    def import_update(self, parsed_update):
+        node_id = self._execute_command(self.merge_node_text,
+                                        parsed_update.bismark_id)
+        if parsed_update.anonymized:
+            signature = parsed_update.anonymization_signature
+        else:
+            signature = ''
+        anonymization_context_id = self._execute_command(
+                self.merge_anonymization_context_text,
+                node_id,
+                signature)
+        session_id = self._execute_command(
+                self.merge_session_text,
+                anonymization_context_id,
+                str(parsed_update.creation_time))
+        (pcap_received, pcap_dropped, iface_dropped) = (-1, -1, -1)
+        try:
+            pcap_received = parsed_update.pcap_received
+            pcap_dropped = parsed_update.pcap_dropped
+            iface_dropped = parsed_update.iface_dropped
+        except:
+            pass
+        update_id = self._execute_command(
+                self.merge_update_text,
+                session_id,
+                parsed_update.sequence_number,
+                parsed_update.packet_series_dropped,
+                parsed_update.flow_table_size,
+                parsed_update.flow_table_expired,
+                parsed_update.flow_table_dropped,
+                parsed_update.dropped_a_records,
+                parsed_update.dropped_cname_records,
+                parsed_update.pcap_received,
+                parsed_update.pcap_dropped,
+                parsed_update.iface_dropped)
+        self._conn.commit()
+
+        for idx, entry in enumerate(parsed_update.addresses):
+            mac_address_id = self._execute_command(self.merge_mac_address_text,
+                                                   anonymization_context_id,
+                                                   entry.mac_address)
+            ip_address_id = self._execute_command(self.merge_ip_address_text,
+                                                  anonymization_context_id,
+                                                  entry.ip_address)
+            remote_id = (parsed_update.address_table_first_id + idx) \
+                    % parsed_update.address_table_size
+            local_address_id = self._execute_command(
+                    self.merge_local_address_text,
+                    remote_id,
+                    update_id,
+                    mac_address_id,
+                    ip_address_id)
+
+        for entry in parsed_update.a_records:
+            local_address_id = self._execute_command(
+                    self.lookup_local_address_for_session,
+                    session_id,
+                    entry.address_id)
+            domain_name_id = self._execute_command(self.merge_domain_name_text,
+                                                   anonymization_context_id,
+                                                   entry.domain)
+            ip_address_id = self._execute_command(self.merge_ip_address_text,
+                                                  anonymization_context_id,
+                                                  entry.ip_address)
+            self._execute_command(self.merge_dns_a_record_text,
+                                  update_id,
+                                  local_address_id,
+                                  domain_name_id,
+                                  ip_address_id)
+
+        for entry in parsed_update.cname_records:
+            local_address_id = self._execute_command(
+                    self.lookup_local_address_for_session,
+                    session_id,
+                    entry.address_id)
+            domain_name_id = self._execute_command(self.merge_domain_name_text,
+                                                   anonymization_context_id,
+                                                   entry.domain)
+            cname_id = self._execute_command(self.merge_domain_name_text,
+                                             anonymization_context_id,
+                                             entry.cname)
+            self._execute_command(self.merge_dns_cname_record_text,
+                                  update_id,
+                                  local_address_id,
+                                  domain_name_id,
+                                  cname_id)
+
+        for entry in parsed_update.flow_table:
+            source_ip_id = self._execute_command(self.merge_ip_address_text,
+                                                 anonymization_context_id,
+                                                 entry.source_ip)
+            destination_ip_id = self._execute_command(
+                    self.merge_ip_address_text,
+                    anonymization_context_id,
+                    entry.destination_ip)
+            self._execute_command(self.merge_flow_text,
+                                  entry.flow_id,
+                                  update_id,
+                                  source_ip_id,
+                                  destination_ip_id,
+                                  entry.transport_protocol,
+                                  entry.source_port,
+                                  entry.destination_port)
+
+        for entry in parsed_update.packet_series:
+            flow_id = self._execute_command(self.lookup_flow_for_session,
+                                            session_id,
+                                            entry.flow_id)
+            self._execute_command(self.merge_packet_text,
+                                  update_id,
+                                  flow_id,
+                                  psycopg2.TimestampFromTicks(entry.timestamp / 1e6),
+                                  entry.size)
+        self._conn.commit()
+
---
+++ schema.sql
@@ -0,0 +1,441 @@
+CREATE TABLE nodes (name varchar PRIMARY KEY);
+
+CREATE TABLE anonymization_contexts (
+    id SERIAL PRIMARY KEY,
+    node_id varchar REFERENCES nodes (name) NOT NULL,
+    signature varchar NOT NULL,
+    UNIQUE (node_id, signature)
+);
+
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    anonymization_context_id integer REFERENCES anonymization_contexts (id) NOT NULL,
+    key varchar NOT NULL,
+    UNIQUE (anonymization_context_id, key)
+);
+
+CREATE TABLE updates (
+    id SERIAL PRIMARY KEY,
+    session_id integer REFERENCES sessions (id) NOT NULL,
+    sequence_number integer NOT NULL,
+    pcap_received integer,
+    pcap_dropped integer,
+    iface_dropped integer,
+    packet_series_dropped integer,
+    flow_table_size integer,
+    flow_table_expired integer,
+    flow_table_dropped integer,
+    dropped_a_records integer,
+    dropped_cname_records integer,
+    UNIQUE (session_id, sequence_number)
+);
+
+CREATE TABLE mac_addresses (
+    id SERIAL PRIMARY KEY,
+    anonymization_context_id integer REFERENCES anonymization_contexts (id) NOT NULL,
+    data varchar NOT NULL,
+    UNIQUE (anonymization_context_id, data)
+);
+
+CREATE TABLE ip_addresses (
+    id SERIAL PRIMARY KEY,
+    anonymization_context_id integer REFERENCES anonymization_contexts (id) NOT NULL,
+    data varchar NOT NULL,
+    UNIQUE (anonymization_context_id, data)
+);
+
+CREATE TABLE domain_names (
+    id SERIAL PRIMARY KEY,
+    anonymization_context_id integer REFERENCES anonymization_contexts (id) NOT NULL,
+    data varchar NOT NULL,
+    UNIQUE (anonymization_context_id, data)
+);
+
+CREATE TABLE local_addresses (
+    id SERIAL PRIMARY KEY,
+    remote_id integer NOT NULL,
+    update_id integer REFERENCES updates (id) NOT NULL,
+    mac_address_id integer REFERENCES mac_addresses (id) NOT NULL,
+    ip_address_id integer REFERENCES ip_addresses (id) NOT NULL,
+    UNIQUE (update_id, mac_address_id, ip_address_id)
+);
+
+CREATE TABLE dns_a_records (
+    id SERIAL PRIMARY KEY,
+    update_id integer REFERENCES updates (id) NOT NULL,
+    local_address_id integer REFERENCES local_addresses (id) NOT NULL,
+    domain_name_id integer REFERENCES domain_names (id) NOT NULL,
+    ip_address_id integer REFERENCES ip_addresses (id) NOT NULL,
+    UNIQUE (update_id, local_address_id, domain_name_id, ip_address_id)
+);
+
+CREATE TABLE dns_cname_records (
+    id SERIAL PRIMARY KEY,
+    update_id integer REFERENCES updates (id) NOT NULL,
+    local_address_id integer REFERENCES local_addresses (id) NOT NULL,
+    domain_name_id integer REFERENCES domain_names (id) NOT NULL,
+    cname_id integer REFERENCES domain_names (id) NOT NULL,
+    UNIQUE (update_id, local_address_id, domain_name_id, cname_id)
+);
+
+CREATE TABLE flows (
+    id SERIAL PRIMARY KEY,
+    remote_id integer NOT NULL,
+    update_id integer REFERENCES updates (id) NOT NULL,
+    source_ip_id integer REFERENCES ip_addresses (id) NOT NULL,
+    destination_ip_id integer REFERENCES ip_addresses (id) NOT NULL,
+    transport_protocol integer NOT NULL,
+    source_port integer NOT NULL,
+    destination_port integer NOT NULL,
+    UNIQUE (remote_id, update_id)
+);
+
+CREATE TABLE packets (
+    id SERIAL PRIMARY KEY,
+    update_id integer REFERENCES updates (id) NOT NULL,
+    flow_id integer REFERENCES flows (id) NOT NULL,
+    timestamp timestamp with time zone NOT NULL,
+    size integer NOT NULL
+);
+
+CREATE FUNCTION merge_node(v_name varchar)
+RETURNS varchar AS $$
+BEGIN
+     INSERT INTO nodes (name) SELECT v_name WHERE NOT EXISTS
+     (SELECT 0 FROM nodes WHERE name = v_name);
+     RETURN v_name;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION merge_anonymization_context(v_node_id varchar, v_signature varchar)
+RETURNS int AS $$
+DECLARE
+     v_id int;
+BEGIN
+     SELECT id INTO v_id FROM anonymization_contexts
+     WHERE node_id = v_node_id AND signature = v_signature;
+     IF NOT found THEN
+         INSERT INTO anonymization_contexts (node_id, signature)
+         VALUES (v_node_id, v_signature)
+         RETURNING id INTO v_id;
+     END IF;
+     RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION merge_session(v_anonymization_context_id int,
+                              v_key varchar)
+RETURNS int AS $$
+DECLARE
+     v_id int;
+BEGIN
+    SELECT id INTO v_id FROM sessions
+    WHERE anonymization_context_id = v_anonymization_context_id
+    AND key = v_key;
+    IF NOT found THEN
+        INSERT INTO sessions (anonymization_context_id, key)
+        VALUES (v_anonymization_context_id, v_key)
+        RETURNING id INTO v_id;
+    END IF;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+merge_update(v_session_id int,
+             v_sequence_number int,
+             v_packet_series_dropped int,
+             v_flow_table_size int,
+             v_flow_table_expired int,
+             v_flow_table_dropped int,
+             v_dropped_a_records int,
+             v_dropped_cname_records int,
+             v_pcap_received int,
+             v_pcap_dropped int,
+             v_iface_dropped int
+             )
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    BEGIN
+        INSERT INTO updates
+        (session_id,
+         sequence_number,
+         packet_series_dropped,
+         flow_table_size,
+         flow_table_expired,
+         flow_table_dropped,
+         dropped_a_records,
+         dropped_cname_records,
+         pcap_received,
+         pcap_dropped,
+         iface_dropped)
+        VALUES
+        (v_session_id,
+         v_sequence_number,
+         v_packet_series_dropped,
+         v_flow_table_size,
+         v_flow_table_expired,
+         v_flow_table_dropped,
+         v_dropped_a_records,
+         v_dropped_cname_records,
+         v_pcap_received,
+         v_pcap_dropped,
+         v_iface_dropped)
+        RETURNING id INTO v_id;
+    EXCEPTION WHEN unique_violation THEN
+        UPDATE updates SET
+         packet_series_dropped = v_packet_series_dropped,
+         flow_table_size = v_flow_table_size,
+         flow_table_expired = v_flow_table_expired,
+         flow_table_dropped = v_flow_table_dropped,
+         dropped_a_records = v_dropped_a_records,
+         dropped_cname_records = v_dropped_cname_records,
+         pcap_received = v_pcap_received,
+         pcap_dropped = v_pcap_dropped,
+         iface_dropped = v_iface_dropped
+        WHERE session_id = v_session_id
+        AND sequence_number = v_sequence_number
+        RETURNING id INTO v_id;
+    END;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+merge_mac_address(v_anonymization_context_id int, v_data varchar)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    SELECT id INTO v_id FROM mac_addresses
+    WHERE anonymization_context_id = v_anonymization_context_id
+    AND data = v_data;
+    IF NOT found THEN
+        INSERT INTO mac_addresses (anonymization_context_id, data)
+        VALUES (v_anonymization_context_id, v_data)
+        RETURNING id INTO v_id;
+    END IF;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+merge_ip_address(v_anonymization_context_id int, v_data varchar)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    SELECT id INTO v_id FROM ip_addresses
+    WHERE anonymization_context_id = v_anonymization_context_id
+    AND data = v_data;
+    IF NOT found THEN
+        INSERT INTO ip_addresses (anonymization_context_id, data)
+        VALUES (v_anonymization_context_id, v_data)
+        RETURNING id INTO v_id;
+    END IF;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+merge_domain_name(v_anonymization_context_id int, v_data varchar)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    SELECT id INTO v_id FROM domain_names
+    WHERE anonymization_context_id = v_anonymization_context_id
+    AND data = v_data;
+    IF NOT found THEN
+        INSERT INTO domain_names (anonymization_context_id, data)
+        VALUES (v_anonymization_context_id, v_data)
+        RETURNING id INTO v_id;
+    END IF;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+merge_local_address(v_remote_id int,
+                    v_update_id int,
+                    v_mac_address_id int,
+                    v_ip_address_id int)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    UPDATE local_addresses SET remote_id = v_remote_id
+    WHERE update_id = v_update_id
+    AND mac_address_id = v_mac_address_id
+    AND ip_address_id = v_ip_address_id
+    RETURNING id INTO v_id;
+    IF NOT found THEN
+        INSERT INTO local_addresses
+        (remote_id,
+         update_id,
+         mac_address_id,
+         ip_address_id)
+        VALUES (v_remote_id,
+                v_update_id,
+                v_mac_address_id,
+                v_ip_address_id)
+        RETURNING id INTO v_id;
+    END IF;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+merge_dns_a_record(v_update_id int,
+                   v_local_address_id int,
+                   v_domain_name_id int,
+                   v_ip_address_id int)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    SELECT id INTO v_id FROM dns_a_records
+    WHERE update_id = v_update_id
+    AND local_address_id = v_local_address_id
+    AND domain_name_id = v_domain_name_id
+    AND ip_address_id = v_ip_address_id;
+    IF NOT found THEN
+        INSERT INTO dns_a_records
+        (update_id,
+         local_address_id,
+         domain_name_id,
+         ip_address_id)
+        VALUES (v_update_id,
+                v_local_address_id,
+                v_domain_name_id,
+                v_ip_address_id)
+        RETURNING id INTO v_id;
+    END IF;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+merge_dns_cname_record(v_update_id int,
+                       v_local_address_id int,
+                       v_domain_name_id int,
+                       v_cname_id int)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    SELECT id INTO v_id FROM dns_cname_records
+    WHERE update_id = v_update_id
+    AND local_address_id = v_local_address_id
+    AND domain_name_id = v_domain_name_id
+    AND cname_id = v_cname_id;
+    IF NOT found THEN
+        INSERT INTO dns_cname_records
+        (update_id,
+         local_address_id,
+         domain_name_id,
+         cname_id)
+        VALUES (v_update_id,
+                v_local_address_id,
+                v_domain_name_id,
+                v_cname_id)
+        RETURNING id INTO v_id;
+    END IF;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+merge_flow(v_remote_id int,
+           v_update_id int,
+           v_source_ip_id int,
+           v_destination_ip_id int,
+           v_transport_protocol int,
+           v_source_port int,
+           v_destination_port int)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    UPDATE flows SET
+    source_ip_id = v_source_ip_id,
+    destination_ip_id = v_destination_ip_id,
+    transport_protocol = v_transport_protocol,
+    source_port = v_source_port,
+    destination_port = v_destination_port
+    WHERE remote_id = v_remote_id
+    AND update_id = v_update_id
+    RETURNING id INTO v_id;
+    IF NOT found THEN
+        INSERT INTO flows
+        (remote_id,
+         update_id,
+         source_ip_id,
+         destination_ip_id,
+         transport_protocol,
+         source_port,
+         destination_port)
+        VALUES (v_remote_id,
+                v_update_id,
+                v_source_ip_id,
+                v_destination_ip_id,
+                v_transport_protocol,
+                v_source_port,
+                v_destination_port)
+        RETURNING id INTO v_id;
+    END IF;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+merge_packet(v_update_id int,
+             v_flow_id int,
+             v_timestamp timestamp with time zone,
+             v_size int)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    INSERT INTO packets (update_id, flow_id, timestamp, size)
+    VALUES (v_update_id, v_flow_id, v_timestamp, v_size)
+    RETURNING id INTO v_id;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+lookup_local_address_for_session(v_session_id int, v_remote_id int)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    SELECT local_addresses.id INTO v_id
+    FROM local_addresses, updates
+    WHERE remote_id = v_remote_id
+    AND session_id = v_session_id
+    AND update_id = updates.id
+    ORDER BY sequence_number DESC
+    LIMIT 1;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION
+lookup_flow_for_session(v_session_id int, v_remote_id int)
+RETURNS int AS $$
+DECLARE
+    v_id int;
+BEGIN
+    SELECT flows.id INTO v_id
+    FROM flows, updates
+    WHERE remote_id = v_remote_id
+    AND session_id = v_session_id
+    AND update_id = updates.id
+    ORDER BY sequence_number DESC
+    LIMIT 1;
+    RETURN v_id;
+END;
+$$ LANGUAGE plpgsql;
