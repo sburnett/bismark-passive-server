5a8c41d523dcb882c6ae60c1245c1818c1131c50
Sam Burnett <srburnet@gmail.com>
2011-10-13 16:49:26 -0400

    Break processor into smaller parts

--- process_sessions.py
+++ process_sessions.py
@@ -41,6 +41,102 @@ class SessionProcessor(object):
     def bytes_per_domain_per_minute(self):
         return self._bytes_per_domain_per_minute
 
+    def process_flow(self, flow):
+        self._flows[flow.flow_id] = flow
+        if flow.source_ip in self._address_map \
+                and not flow.destination_ip_anonymized:
+            key = (self._address_map[flow.source_ip], flow.destination_ip)
+        elif flow.destination_ip in self._address_map \
+                and not flow.source_ip_anonymized:
+            key = (self._address_map[flow.destination_ip], flow.source_ip)
+        else:
+            return
+        self._flow_ip_map.setdefault(key, set())
+        if key in self._dns_map_ip:
+            self._flow_ip_map[key].update(self._dns_map_ip[key])
+
+    def process_a_record(self, a_record, a_packet):
+        if a_record.anonymized:
+            return
+        domain_key = (a_record.address_id, a_record.domain)
+        self._dns_a_map_domain.setdefault(domain_key, [])
+        self._dns_a_map_domain[domain_key].append(a_record)
+        for domain, pattern in self._whitelist:
+            if pattern.search(a_record.domain) is not None:
+                ip_key = (a_record.address_id, a_record.ip_address)
+                domain_record = (domain,
+                                 a_packet.timestamp,
+                                 a_packet.timestamp + a_record.ttl)
+                self._dns_map_ip.setdefault(ip_key, set())
+                self._dns_map_ip[ip_key].add(domain_record)
+                if ip_key in self._flow_ip_map:
+                    self._flow_ip_map[ip_key].add(domain_record)
+
+    def process_cname_record(self, cname_record, packet_series):
+        if cname_record.anonymized:
+            return
+        try:
+            cname_packet = packet_series[cname_record.packet_id]
+        except IndexError:
+            return
+        domain_key = (cname_record.address_id, cname_record.cname)
+        a_records = self._dns_a_map_domain.get(domain_key)
+        if a_records is None:
+            return
+        for domain, pattern in self._whitelist:
+            if pattern.search(cname_record.domain) is not None:
+                for a_record in a_records:
+                    try:
+                        a_packet = packet_series[a_record.packet_id]
+                    except IndexError:
+                        continue
+                    start_timestamp = max(cname_packet.timestamp,
+                                          a_packet.timestamp)
+                    end_timestamp = min(
+                            cname_packet.timestamp + cname_record.ttl,
+                            a_packet.timestamp + a_record.ttl)
+                    if start_timestamp > end_timestamp:
+                        continue
+                    ip_key = (a_record.address_id, a_record.ip_address)
+                    domain_record = (domain, start_timestamp, end_timestamp)
+                    self._dns_map_ip.setdefault(ip_key, set())
+                    self._dns_map_ip[ip_key].add(domain_record)
+                    if ip_key in self._flow_ip_map:
+                        self._flow_ip_map[ip_key].add(domain_record)
+
+    def process_packet(self, packet):
+        rounded_timestamp = packet.timestamp.replace(second=0, microsecond=0)
+        self._bytes_per_minute.setdefault(rounded_timestamp, 0)
+        self._bytes_per_minute[rounded_timestamp] += packet.size
+
+        flow = self._flows.get(packet.flow_id)
+        if flow is not None:
+            port_key = None
+            if flow.source_ip in self._address_map:
+                port_key = (rounded_timestamp, flow.destination_port)
+            elif flow.destination_ip in self._address_map:
+                port_key = (rounded_timestamp, flow.source_port)
+            if port_key is not None:
+                self._bytes_per_port_per_minute.setdefault(port_key, 0)
+                self._bytes_per_port_per_minute[port_key] += packet.size
+
+            key = None
+            if flow.source_ip in self._address_map \
+                    and not flow.destination_ip_anonymized:
+                key = (self._address_map[flow.source_ip], flow.destination_ip)
+            elif flow.destination_ip in self._address_map \
+                    and not flow.source_ip_anonymized:
+                key = (self._address_map[flow.destination_ip], flow.source_ip)
+            if key is not None and key in self._flow_ip_map:
+                for domain, start_time, end_time in self._flow_ip_map[key]:
+                    if packet.timestamp < start_time \
+                            or packet.timestamp > end_time:
+                        continue
+                    domain_key = (rounded_timestamp, domain)
+                    self._bytes_per_domain_per_minute.setdefault(domain_key, 0)
+                    self._bytes_per_domain_per_minute[domain_key] \
+                            += packet.size
+
     def process_update(self, update):
         if update.sequence_number != self._last_sequence_number_processed + 1:
             return False
@@ -54,111 +150,20 @@ class SessionProcessor(object):
             self._address_map[address.ip_address] = index
 
         for flow in update.flow_table:
-            self._flows[flow.flow_id] = flow
-            if flow.source_ip in self._address_map \
-                    and not flow.destination_ip_anonymized:
-                key = (self._address_map[flow.source_ip], flow.destination_ip)
-            elif flow.destination_ip in self._address_map \
-                    and not flow.source_ip_anonymized:
-                key = (self._address_map[flow.destination_ip], flow.source_ip)
-            else:
-                continue
-            self._flow_ip_map.setdefault(key, set())
-            if key in self._dns_map_ip:
-                self._flow_ip_map[key].update(self._dns_map_ip[key])
+            self.process_flow(flow)
 
         for a_record in update.a_records:
-            if a_record.anonymized:
-                continue
-            domain_key = (a_record.address_id, a_record.domain)
-            self._dns_a_map_domain.setdefault(domain_key, [])
-            self._dns_a_map_domain[domain_key].append(a_record)
             try:
                 a_packet = update.packet_series[a_record.packet_id]
             except IndexError:
                 continue
-            for domain, pattern in self._whitelist:
-                if pattern.search(a_record.domain) is not None:
-                    ip_key = (a_record.address_id, a_record.ip_address)
-                    domain_record = (domain,
-                                     a_packet.timestamp,
-                                     a_packet.timestamp + a_record.ttl)
-                    self._dns_map_ip.setdefault(ip_key, set())
-                    self._dns_map_ip[ip_key].add(domain_record)
-                    if ip_key in self._flow_ip_map:
-                        self._flow_ip_map[ip_key].add(domain_record)
+            self.process_a_record(a_record, a_packet)
 
         for cname_record in update.cname_records:
-            if cname_record.anonymized:
-                continue
-            try:
-                cname_packet = update.packet_series[cname_record.packet_id]
-            except IndexError:
-                continue
-            domain_key = (cname_record.address_id, cname_record.cname)
-            a_records = self._dns_a_map_domain.get(domain_key)
-            if a_records is None:
-                continue
-            for domain, pattern in self._whitelist:
-                if pattern.search(cname_record.domain) is not None:
-                    for a_record in a_records:
-                        try:
-                            a_packet = update.packet_series[a_record.packet_id]
-                        except IndexError:
-                            continue
-                        start_timestamp = max(cname_packet.timestamp,
-                                              a_packet.timestamp)
-                        end_timestamp = min(
-                                cname_packet.timestamp + cname_record.ttl,
-                                a_packet.timestamp + a_record.ttl)
-                        if start_timestamp > end_timestamp:
-                            continue
-                        ip_key = (a_record.address_id, a_record.ip_address)
-                        domain_record = (domain,
-                                         start_timestamp,
-                                         end_timestamp)
-                        self._dns_map_ip.setdefault(ip_key, set())
-                        self._dns_map_ip[ip_key].add(domain_record)
-                        if ip_key in self._flow_ip_map:
-                            self._flow_ip_map[ip_key].add(domain_record)
+            self.process_cname_record(cname_record, update.packet_series)
 
         for packet in update.packet_series:
-            rounded_timestamp = packet.timestamp.replace(second=0,
-                                                         microsecond=0)
-            self._bytes_per_minute.setdefault(rounded_timestamp, 0)
-            self._bytes_per_minute[rounded_timestamp] += packet.size
-
-            flow = self._flows.get(packet.flow_id)
-            if flow is not None:
-                port_key = None
-                if flow.source_ip in self._address_map:
-                    port_key = (rounded_timestamp, flow.destination_port)
-                elif flow.destination_ip in self._address_map:
-                    port_key = (rounded_timestamp, flow.source_port)
-                if port_key is not None:
-                    self._bytes_per_port_per_minute.setdefault(port_key, 0)
-                    self._bytes_per_port_per_minute[port_key] += packet.size
-
-                key = None
-                if flow.source_ip in self._address_map \
-                        and not flow.destination_ip_anonymized:
-                    key = (self._address_map[flow.source_ip],
-                           flow.destination_ip)
-                elif flow.destination_ip in self._address_map \
-                        and not flow.source_ip_anonymized:
-                    key = (self._address_map[flow.destination_ip],
-                           flow.source_ip)
-                if key is not None and key in self._flow_ip_map:
-                    for domain, start_time, end_time in self._flow_ip_map[key]:
-                        if packet.timestamp < start_time \
-                                or packet.timestamp > end_time:
-                            continue
-                        domain_key = (rounded_timestamp, domain)
-                        self._bytes_per_domain_per_minute.setdefault(
-                                domain_key,
-                                0)
-                        self._bytes_per_domain_per_minute[domain_key] \
-                                += packet.size
+            self.process_packet(packet)
 
         self._last_sequence_number_processed = update.sequence_number
         return True
