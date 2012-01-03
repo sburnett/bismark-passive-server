3aabca900b306d98cb885c234f9b0953dd3e208b
Bhanu Vattikonda <bvattikonda@dp4.gtnoise.net>
2012-01-03 19:29:04 +0000

    Including dates of domain access
    Modified the database scheme to now include the dates on which each domain is
    accessed by each device.

--- domain_processor.py
+++ domain_processor.py
@@ -3,7 +3,6 @@ import re
 from session_processor import SessionProcessor
 from postgres_session_processor import PostgresProcessorCoordinator 
 import utils
-import inspect
 
 class DomainSessionProcessor(SessionProcessor):
     def __init__(self):
@@ -17,31 +16,37 @@ class DomainSessionProcessor(SessionProcessor):
 
         for a_record in update.a_records:
             self.process_a_record(context, update.bismark_id,\
-                a_record)
+                a_record, update.timestamp)
         for cname_record in update.cname_records:
             self.process_cname_record(context, update.bismark_id,\
-                cname_record)
+                cname_record, update.timestamp)
     
-    def process_a_record(self, context, bismark_id, a_record):
+    def process_a_record(self, context, bismark_id, a_record, timestamp):
         if a_record.anonymized:
             return
         mac_address = context.address_id_map[a_record.address_id]
-        device_key = bismark_id, mac_address
         domain = a_record.domain
-        context.domains_accessed[device_key].add(domain)
+        record_key = bismark_id, mac_address, domain
+        context.domains_accessed[record_key].append(\
+            datetime.date(timestamp.replace(hour = 0, minute = 0,\
+            second = 0, microsecond = 0)))
     
-    def process_cname_record(self, context, bismark_id, cname_record):
-        if cname_record.anonymized:
+    def process_cname_record(self, context, bismark_id, c_record, timestamp):
+        if c_record.anonymized:
             return
-        mac_address = context.address_id_map[cname_record.address_id]
-        device_key = bismark_id, mac_address
-        domain = cname_record.domain
-        context.domains_accessed[device_key].add(domain)
+        mac_address = context.address_id_map[c_record.address_id]
+        domain = c_record.domain
+        record_key = bismark_id, mac_address, domain
+        context.domains_accessed[record_key].append(\
+            datetime.date(timestamp.replace(hour = 0, minute = 0,\
+            second = 0, microsecond = 0)))
                 
 class DomainProcessorCoordinator(PostgresProcessorCoordinator):
+    # Store the dates on which each domain was visited
     persistent_state = dict(
             address_id_map = (dict, None),
-            domains_accessed = (utils.initialize_set_dict, utils.update_set),
+            domains_accessed = (utils.initialize_list_dict,\
+                utils.sum_dicts),
             )
     ephemeral_state = dict()
     
