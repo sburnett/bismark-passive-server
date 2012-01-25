ff0f9efae1197299d6be3434ee2214062c65082f
Sam Burnett <srburnet@gmail.com>
2012-01-25 23:35:27 +0000

    Move update statistics plot to DataAvailabilityProcessor

--- update_statistics_plot.py
+++ update_statistics_plot.py
@@ -7,57 +7,33 @@ from os.path import join
 
 from harness import Harness, main
 import harness
-from update_statistics_processor import UpdateStatisticsSessionProcessor
+from update_statistics_processor import DataAvailabilityProcessor
 
 OUTAGE_TIMEOUT = datetime.timedelta(seconds=60)
 
 class PlotUpdateStatisticsHarness(Harness):
-    processors = [UpdateStatisticsSessionProcessor]
+    processors = [DataAvailabilityProcessor]
 
-    def __init__(self):
-        super(PlotUpdateStatisticsHarness, self).__init__()
-
-    def plot_updates(self, updates, filename, max_age=None):
+    def plot_updates(self, availability_intervals, filename, max_age=None):
         current_time = datetime.datetime.utcnow()
         if max_age is None:
             oldest = datetime.datetime.min
         else:
             oldest = current_time - max_age
         
-        node_ids = set()
-        for update in updates:
-            node_ids.add(update.node_id)
-        node_ids = list(node_ids)
+        outages = dict()
+        for node_id, intervals in availability_intervals.items():
+            outages[node_id] = map(lambda (lower, upper): upper, intervals)
 
-        availabilities = defaultdict(list)
-        outages = defaultdict(list)
-        current_node_id = None
-        for update in updates:
-            if update.eventstamp <= oldest:
-                continue
-            if update.node_id != current_node_id:
-                if current_node_id is not None:
-                    availabilities[current_node_id].append((lower, upper))
-                current_node_id = update.node_id
-                lower = update.eventstamp
-                upper = update.eventstamp
-            if update.eventstamp - upper >= OUTAGE_TIMEOUT:
-                print 'Outage:', update.node_id, update.eventstamp - upper
-                availabilities[update.node_id].append((lower, upper))
-                outages[update.node_id].append(upper)
-                lower = update.eventstamp
-            upper = update.eventstamp
-        if current_node_id is not None:
-            availabilities[update.node_id].append((lower, upper))
-
+        node_ids = sorted(availability_intervals.keys())
         indices = {}
         for node_id in node_ids:
             indices[node_id] = node_ids.index(node_id)
 
-        availability_intervals = []
-        for node_id, intervals in availabilities.items():
+        availability_lines = []
+        for node_id, intervals in availability_intervals.items():
             for lower, upper in intervals:
-                availability_intervals.append((indices[node_id], lower, upper))
+                availability_lines.append((indices[node_id], lower, upper))
         outage_points = []
         for node_id, outage_times in outages.items():
             for beginning in outage_times:
@@ -68,8 +44,8 @@ class PlotUpdateStatisticsHarness(Harness):
         fig = plt.figure()
         ax = fig.add_subplot(111)
         plt.yticks(range(len(node_ids)), node_ids, family='monospace')
-        if len(availability_intervals) > 0:
-            ax.hlines(*zip(*availability_intervals), lw=2)
+        if len(availability_lines) > 0:
+            ax.hlines(*zip(*availability_lines), lw=2)
         if len(outage_points) > 0:
             ax.vlines(*zip(*outage_points), lw=2, color='r')
         ax.set_xlabel('Date')
@@ -96,18 +72,17 @@ class PlotUpdateStatisticsHarness(Harness):
         plt.savefig(filename)
 
     def process_results(self, global_context):
-        updates = list(chain.from_iterable(global_context.update_statistics))
-        updates.sort(key=lambda u: (u.node_id, u.eventstamp))
         print 'Plotting daily status'
-        self.plot_updates(updates,
+        self.plot_updates(global_context.availability_intervals,
                           join(harness.options.plots_directory, 'status_daily.png'),
                           datetime.timedelta(days=1))
         print 'Plotting weekly status'
-        self.plot_updates(updates,
+        self.plot_updates(global_context.availability_intervals,
                           join(harness.options.plots_directory, 'status_weekly.png'),
                           datetime.timedelta(days=7))
         print 'Plotting overall status'
-        self.plot_updates(updates, join(harness.options.plots_directory, 'status.png'))
+        self.plot_updates(global_context.availability_intervals,
+                          join(harness.options.plots_directory, 'status.png'))
 
 if __name__ == '__main__':
     main(PlotUpdateStatisticsHarness)
