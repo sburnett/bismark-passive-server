f4396a37350d6e13c39034c8e5ddb2739ed918a0
Sam Burnett <srburnet@gmail.com>
2012-01-25 22:05:19 +0000

    Add many options to node plot

--- node_plot.py
+++ node_plot.py
@@ -5,16 +5,24 @@ from os.path import join
 import harness
 
 class NodePlotHarness(harness.Harness):
-    def plot(self, plotter, context, filename, node_ids, figsize=(15,20), limits=(None, None)):
+    def plot(self, plotter, context, filename, node_ids,
+                figsize=(15,20),
+                limits=(None, None),
+                title=True,
+                availability=True,
+                timestamp=True,
+                autoformat=True,
+                tight_layout=True):
         node_ids.sort()
         fig = plt.figure(1, figsize=figsize)
         fig.clear()
-        plt.text(1, 0, 'Generated %s' % datetime.now(),
-                 fontsize=8,
-                 color='gray',
-                 transform=fig.transFigure,
-                 verticalalignment='bottom',
-                 horizontalalignment='right')
+        if timestamp:
+            plt.text(1, 0, 'Generated %s' % datetime.now(),
+                     fontsize=8,
+                     color='gray',
+                     transform=fig.transFigure,
+                     verticalalignment='bottom',
+                     horizontalalignment='right')
         for row, node_id in enumerate(node_ids):
             try:
                 ax = plt.subplot(len(node_ids), 1, row, sharex=first_axis)
@@ -25,10 +33,14 @@ class NodePlotHarness(harness.Harness):
                 ax.set_xlim(left=left)
             if right is not None:
                 ax.set_xlim(right=right)
-            plt.title(node_id)
-            for lower, upper in context.availability_intervals[node_id]:
-                ax.axvspan(lower, upper, facecolor='y', alpha=0.3, linewidth=0.0)
+            if title:
+                plt.title(node_id)
+            if availability:
+                for lower, upper in context.availability_intervals[node_id]:
+                    ax.axvspan(lower, upper, facecolor='y', alpha=0.3, linewidth=0.0)
             plotter(context, node_id, ax, limits)
-        fig.autofmt_xdate(bottom=0)
-        plt.tight_layout()
+        if autoformat:
+            fig.autofmt_xdate(bottom=0)
+        if tight_layout:
+            plt.tight_layout()
         plt.savefig(join(harness.options.plots_directory, filename))
