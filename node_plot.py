ddb9a7fda20db00e4fb237247213bad486388af2
Sam Burnett <srburnet@gmail.com>
2012-01-26 20:53:22 +0000

    Add class for plotting many plots, each for a single node.

--- node_plot.py
+++ node_plot.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from errno import EEXIST
 import matplotlib.pyplot as plt
+from os import makedirs
 from os.path import join
 
 import harness
@@ -44,3 +46,47 @@ class NodePlotHarness(harness.Harness):
         if tight_layout:
             plt.tight_layout()
         plt.savefig(join(harness.options.plots_directory, filename))
+
+class PlotPerNodeHarness(harness.Harness):
+    def plot(self, plotter, context, directory, node_ids,
+                extension='png',
+                figsize=(11, 8.5),
+                limits=(None, None),
+                title=True,
+                availability=True,
+                timestamp=True,
+                autoformat=True,
+                tight_layout=True):
+        try:
+            makedirs(join(harness.options.plots_directory, directory))
+        except OSError, e:
+            if e.errno != EEXIST:
+                raise
+        for node_id in node_ids:
+            fig = plt.figure(1, figsize=figsize)
+            fig.clear()
+            left, right = limits
+            if left is not None:
+                plt.xlim(xmin=left)
+            if right is not None:
+                plt.xlim(xmax=right)
+            if title:
+                plt.title(node_id)
+            if availability:
+                for lower, upper in context.availability_intervals[node_id]:
+                    plt.axvspan(lower, upper, facecolor='0.9', linewidth=0.0)
+            plotter(context, node_id, limits)
+            if autoformat:
+                fig.autofmt_xdate(bottom=0)
+            if timestamp:
+                plt.text(1, 0, 'Generated %s' % datetime.now(),
+                         fontsize=8,
+                         color='gray',
+                         transform=fig.transFigure,
+                         verticalalignment='bottom',
+                         horizontalalignment='right')
+            if tight_layout:
+                plt.tight_layout()
+            plt.savefig(join(harness.options.plots_directory,
+                             directory,
+                             '%s.%s' % (node_id, extension)))
