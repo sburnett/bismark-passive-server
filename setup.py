fe78d3d62b29f06d3e90c99cbb602d991a350b61
Sam Burnett <srburnet@gmail.com>
2012-02-02 18:17:45 +0000

    Add build script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name='bismarkpassive',
+        version='1.0',
+        packages=[
+            'bismarkpassive',
+            'bismarkpassive.processors',
+            'bismarkpassive.harnesses'
+            ]
+        )
